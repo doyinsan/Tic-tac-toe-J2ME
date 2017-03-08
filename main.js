94d71bb366a0043860fd63549bb1a1d618a36ad9
Dele oyinsan <ooyinsan@spartaglobal.co>
2017-03-08 10:53:10 +0000

    tidy up JS indentation

--- main.js
+++ main.js
@@ -1,42 +1,127 @@
 $(document).ready(function(){
+  var oneClick = 0;
 
-  var D = D;
-  var A = A;
-  var oneClick = oneClick;
-
-  $('#row1').click(function() {
-
-    if ($('#1').addClass('D activate'))  {
+  $('#one').click(function() {
+    if ($('#one').addClass('D activate'))  {
       oneClick--;
-    }else
-  $('#1').addClass('A activate');
-    {
-      oneClick--;
+    } else {
+      $('#one').addClass('A activate');
     }
+    oneClick--;
     alert('O has won the game. Start a new game');
 
     $('#row1 td').removeClass('disable');
     $('#row1 td').removeClass('D');
     $('#row1 td').removeClass('A');
-  }
+  });
 
-);
-$('#row2').click(function() {
-
-  if ($('#1').addClass('D activate'))  {
+  $('#two').click(function() {
+    if ($('#two').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
     oneClick--;
-  }else
-$('#1').addClass('A activate');
-  {
+    alert('O has won the game. Start a new game');
+
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
+
+  $('#three').click(function() {
+    if ($('#three').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
     oneClick--;
-  }
-  alert('O has won the game. Start a new game');
+    alert('O has won the game. Start a new game');
 
-  $('#row1 td').removeClass('disable');
-  $('#row1 td').removeClass('D');
-  $('#row1 td').removeClass('A');
-}
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
 
-);
+  $('#four').click(function() {
+    if ($('#four').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
+    oneClick--;
+    alert('O has won the game. Start a new game');
 
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
+  $('#five').click(function() {
+    if ($('#five').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
+    oneClick--;
+    alert('O has won the game. Start a new game');
+
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
+
+  $('#six').click(function() {
+    if ($('#six').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
+    oneClick--;
+    alert('O has won the game. Start a new game');
+
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
+
+  $('#seven').click(function() {
+    if ($('#seven').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
+    oneClick--;
+    alert(' has won the game. Start a new game');
+
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
+
+  $('#eight').click(function() {
+    if ($('#eight').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
+    oneClick--;
+    alert('O has won the game. Start a new game');
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
+
+  $('#nine').click(function() {
+    if ($('#nine').addClass('D activate'))  {
+      oneClick--;
+    } else {
+      $('#1').addClass('A activate');
+    }
+    oneClick--;
+    alert('O has won the game. Start a new game');
+
+    $('#row1 td').removeClass('disable');
+    $('#row1 td').removeClass('D');
+    $('#row1 td').removeClass('A');
+  });
 });
